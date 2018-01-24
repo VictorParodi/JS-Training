d42eed15833c7ab7881e13d7d17a3eee4048f898
Victor Parodi <victorparodi2@gmail.com>
2018-01-24 18:41:26 -0500

    Loop's training

---
+++ sandbox/loops.js
@@ -0,0 +1,37 @@
+{
+ // --- for of loop
+ // let users = ['Kate', 'Allison', 'Tom'];
+ // for(let user of users) {
+ //  if(user === 'Allison') {
+ //   console.log(`${user} is awesome!`);
+ //   continue;
+ //  }
+ //  console.log(user);
+ // }
+
+
+ // --- while loop
+ // let iterator = 0;
+ // while(iterator < 10) {
+ //  if(iterator === 3) {
+ //   console.log(`${iterator} break the loop`);
+ //   break;
+ //  }
+ //  console.log(iterator);
+ //  iterator++;
+ // }
+
+// --- for in loop
+// let user = {
+//  name: 'Allison',
+//  lastname: 'Mack',
+//  age: 25
+// }
+// for(let prop in user) {
+//  if(prop === 'lastname') {
+//   console.log('The loop is going to break');
+//   break;
+//  }
+//  console.log(user[prop]);
+// }
+}
