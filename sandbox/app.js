06a6edaa47541fca82657bd30087cddce0f2d805
Victor <victor.parodi.montero@gmail.com>
2018-01-23 21:10:47 -0500

    Métodos de String y Array

---
+++ sandbox/app.js
@@ -0,0 +1,77 @@
+{
+  'use strict';
+
+  let number1 = 5,
+      number2 = 4,
+      name = 'Allison',
+      lastname = 'Mack',
+      users = ['Lia', 'Ana', 'Tom', 'Chris'],
+      numbers = [1, 4, 3, 5, 2, 10];
+
+  // ========== String Methods ==========
+  // --- concat()
+  // console.log(name.concat(' ', lastname));
+
+  // --- indexOf()
+  // console.log(name.indexOf('n'));
+
+  // --- chartAt
+  // console.log(name.charAt(6));
+  
+  // --- replace()
+  // let newName = name.concat(' ', lastname)
+  // console.log(newName.replace('Mack', 'Winslet'));
+
+  // --- includes()
+  // console.log(name.includes('l'));
+
+
+  // ========== Array Methods ==========
+  // --- Array.isArray();
+  // console.log(Array.isArray(users));
+
+  // --- indexOf()
+  // console.log(users.indexOf('Ana'));
+
+  // --- reverse()
+  // console.log(users.reverse());
+  // console.log(users);
+  
+  
+  // --- sort()
+  // console.log(users.sort());
+  // console.log(users);
+  // console.log(numbers);
+  // console.log(numbers.sort());
+  // console.log(
+  //   numbers.sort((a, b) => {
+  //     return a - b;
+  //   })
+  // );
+
+  // --- find()
+  // console.log(
+  //   users.find((user) => {
+  //     return user.length < 4;
+  //   })
+  // );
+
+  // --- splice
+  // console.log(users);
+  // let tempo = users.splice(0,1, 'Erick');
+  // console.log(tempo);
+  // console.log(users);
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+
+}
