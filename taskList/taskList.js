898bfdb834810f4763fdceb7364ace936e3e6c29
Victor <victor.parodi.montero@gmail.com>
2018-02-05 22:59:13 -0500

    Remove and filter tasks

--- taskList/taskList.js
+++ taskList/taskList.js
@@ -9,8 +9,13 @@
 
  function loadEventListeners() {
   form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
  }
+
  
+ // Add task
  function addTask(e) {
   if(taskInput.value === '') {
    alert('Add task');
@@ -24,7 +29,6 @@
    link.innerHTML =  '<i class="fa fa-remove"></i>';
    
    li.appendChild(link);
-   console.log(li);
 
    taskList.appendChild(li);
 
@@ -33,4 +37,39 @@
 
   e.preventDefault();
  } // addTask
+
+
+ // Remove task
+ function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+ }
+
+
+ // Clear tasks
+ function clearTasks(e) {
+  const tasks = document.querySelectorAll('.collection-item');
+  
+  for(let task of tasks) {
+    task.remove();
+  }
+ }
+
+
+ // Filter tasks
+ function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  
+   const tasks = document.querySelectorAll('.collection-item');
+
+   tasks.forEach(function(task) {
+    if(task.firstChild.textContent.toLowerCase().includes(text) ) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+   });
+ }
+
 } // scope
