5846c8ea4105270df341f96f5764895bacabd496
Victor Parodi <victorparodi2@gmail.com>
2018-02-06 20:17:45 -0500

    Add and delete tasks to/from localStorage

--- taskList/taskList.js
+++ taskList/taskList.js
@@ -12,6 +12,30 @@
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTasks);
   filter.addEventListener('keyup', filterTasks);
+  document.addEventListener('DOMContentLoaded', getTasks);
+ }
+
+ // Get tasks from LS
+ function getTasks() {
+  let tasks;
+
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'collection-item');
+    li.appendChild(document.createTextNode(task));
+
+    const link = document.createElement('a');
+    link.setAttribute('class', 'delete-item secondary-content');
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+  });
  }
 
  
@@ -32,19 +56,57 @@
 
    taskList.appendChild(li);
 
+   storeInLocalStorage(taskInput.value);
+
    taskInput.value = '';
   }
 
   e.preventDefault();
- } // addTask
+ }
+
+
+ // Store in local Store
+ function storeInLocalStorage(task) {
+  let tasks;
+
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+ }
 
 
  // Remove task
  function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
     e.target.parentElement.parentElement.remove();
+
+    removeTaskFromLS(e.target.parentElement.parentElement);
   }
  }
+
+ function removeTaskFromLS(taskItem) {
+  let tasks;
+
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index) {
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1); 
+    }
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  });
+ };
 
 
  // Clear tasks
@@ -54,6 +116,12 @@
   for(let task of tasks) {
     task.remove();
   }
+
+  clearTasksFromLS();
+ }
+
+ function clearTasksFromLS() {
+   localStorage.clear();
  }
 
 
