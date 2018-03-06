a682e43bf571a8ee86a55fcd9088b4d38165313c
Victor <victor.parodi.montero@gmail.com>
2018-03-05 23:12:16 -0500

    Building a custom HTTP request library: add fix

--- easyHttp/app2.js
+++ easyHttp/app2.js
@@ -1,6 +1,6 @@
 const http = new easyHTTP;
 
-http.get('https://jsonplaceholder.typicode.com/postsx', function(err, posts) {
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
   err? console.log(err) : console.log(posts);
 });
 
