2a5150abba32824acd53f506a8e566681ef19274
Victor <victor.parodi.montero@gmail.com>
2018-03-06 21:12:41 -0500

    Post request for custom http library

--- easyHttp/app2.js
+++ easyHttp/app2.js
@@ -1,6 +1,18 @@
 const http = new easyHTTP;
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  err? console.log(err) : console.log(posts);
+// Get request code
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   err? console.log(err) : console.log(posts);
+// });
+
+// Post request code
+const data = {
+  name: 'Allison Mack',
+  username: 'Ali Mack',
+  email: 'ali@gmail.com'
+}
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, user) {
+  err? console.log(err) : console.log(user);
 });
 
--- easyHttp/easyHttp2.js
+++ easyHttp/easyHttp2.js
@@ -2,18 +2,34 @@ function easyHTTP() {
   this.http = new XMLHttpRequest();
 }
 
-easyHTTP.prototype.get = function(url, callback) {
-  this.http.open('GET', url, true);
+// Get request
+// easyHTTP.prototype.get = function(url, callback) {
+//   this.http.open('GET', url, true);
 
-  let self = this;
+//   let self = this;
+
+//   this.http.onload = function() {
+//     if(self.http.status === 200) {
+//       callback(null, self.http.responseText);
+//     } else {
+//       callback(`There is an error: ${self.http.status}`);
+//     }
+//   }
+
+//   this.http.send();
+// }
+
+// Post request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this.http;
 
   this.http.onload = function() {
-    if(self.http.status === 200) {
-      callback(null, self.http.responseText);
-    } else {
-      callback(`There is an error: ${self.http.status}`);
-    }
+    callback(null, self.responseText);
   }
 
-  this.http.send();
+  this.http.send(JSON.stringify(data));
 }
