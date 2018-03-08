b16ab3d8aff5ffbdc8de1db1c9a24b5b7db20b26
Victor <victor.parodi.montero@gmail.com>
2018-03-08 00:27:20 -0500

    Handling data with fetch API

--- fetchAPI/app2.js
+++ fetchAPI/app2.js
@@ -2,15 +2,67 @@ const btn_0 = document.getElementById('button1');
 
 btn_0.addEventListener('click', getText);
 
+// Get local text file data
 function getText() {
   fetch('test.txt')
     .then(function(res) {
       return res.text();
     })
     .then(function(data) {
-      console.log(data);
+      const output = document.querySelector('#output');
+      output.innerHTML = data;
     })
     .catch(function(err) {
       console.log(err);
+    });
+}
+
+// Get local json file data
+const btn_1 = document.getElementById('button2');
+
+btn_1.addEventListener('click', getJSON);
+
+function getJSON() {
+  fetch('posts.json')
+    .then(function(res) {
+      return res.json()
+    })
+    .then(function(data) {
+      let out = '';
+
+      data.forEach(function(post) {
+        out += `<li> ${post.title} </li>`;
+      });
+
+      const output = document.querySelector('#output');
+      output.innerHTML = out;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get external data from API
+const btn_2 = document.getElementById('button3');
+
+btn_2.addEventListener('click', getExternal);
+
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      let out = '';
+
+      data.forEach(function(user) {
+        out += `<li> ${user.login} </li>`
+      });
+
+      const output = document.querySelector('#output');
+      output.innerHTML = out;
+    })
+    .catch(function(err) {
+      console.log('ERROR:', err);
     });
 }
